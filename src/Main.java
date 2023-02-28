5dd8a068e103524ac50fb3dfba29039d22b4c5fd
sunny <sunnyroy8084401051@gmail.com>
2023-02-28 13:14:38 +0530

    commit :

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 /*
 Question : Write a program to convert the given numbers of days into year,month and weeks.
  */
-
 import java.util.Scanner;
 public class Main {
     static void NumberOfMonth(int days){
-        int year = 365;
+        int year = 365,result,weeks,day1;
         if (days >= year) {
-
+            result = days / year;
+            days = days % year;
+            System.out.println( "Number of years : " + result);
+            weeks = days / 7;
+            days = days % 7;
+            System.out.println("Number of weeks : " + weeks);
+            day1 = days;
+            System.out.println("Numbers of days :" + day1);
         }else
             System.out.println("It is not year because a year contains 365 days. plz enter the valid number :");
     }
